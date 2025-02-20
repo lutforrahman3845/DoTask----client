039fd325abbd99c4dff16f448f4b922c15aed6a0
Lutfor Rahman <lutforr3845@gmail.com>
2025-02-21 01:35:33 +0600

    create simple bannar

---
+++ src/Component/Banner.jsx
@@ -0,0 +1,16 @@
+const Banner = () => {
+    return (
+      <div className="relative w-full h-64  flex items-center justify-center text-Primary dark:text-white text-center px-6">
+        <div>
+          <h1 className="text-3xl font-bold font-poppins">Effortless Task Management</h1>
+          <p className="mt-2 text-lg max-w-4xl mx-auto font-normal font-roboto">
+            Add tasks easily and drag them between categories—To-Do, In Progress, and Done.  
+            Stay organized with a simple, intuitive workflow.
+          </p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Banner;
+  
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,9 @@
+import Banner from "../Component/Banner";
 
 const Home = () => {
     return (
         <div>
-            Home
+            <Banner/>
         </div>
     );
 };
