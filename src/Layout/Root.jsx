229fda833fe6a6bd95a96dff381a0d793b0101ea
Lutfor Rahman <lutforr3845@gmail.com>
2025-02-20 22:13:37 +0600

    add root layout , routes && navbar

---
+++ src/Layout/Root.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router";
+import Navbar from "../Components/Navbar";
+
+const Root = () => {
+  return (
+    <div className="container mx-auto">
+      <Navbar/>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Root;
